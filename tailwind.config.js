62e099e7f28fc2095a87b8a025245c640a091f64
RijuBera <sumanb.0303@gmail.com>
2024-02-15 17:46:35 +0530

    all

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+      deepblue:"#02042a"
+      },
+    },
+  },
+  plugins: [],
+}
